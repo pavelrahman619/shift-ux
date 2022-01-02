e31c1ac4ef5a8083f8d16509c1dd47831d94e230
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-01-02 16:13:38 +0600

    added homepage stuff

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <!-- <v-app>
     <v-app-bar
       app
       color="primary"
@@ -40,21 +40,135 @@
     <v-main>
       <HelloWorld/>
     </v-main>
-  </v-app>
+  </v-app> -->
+
+  <div id="app">
+    <span class="bg"></span>
+    <v-app>
+      <v-navigation-drawer right hide-overlay="true" v-model="drawer" app>
+        <!--  -->
+      </v-navigation-drawer>
+
+      <v-app-bar app color="transparent">
+        <a href="#">
+          <img
+            class="mr-3"
+            :src="require('../public/images/shift-logo.png')"
+            height="40"
+        /></a>
+
+        <!-- <v-toolbar-title>Application</v-toolbar-title> -->
+        <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      </v-app-bar>
+
+      <v-main>
+        <!--  -->
+        <div class="container">
+          <div>
+            <h1><b>Let's change the world one campaign at a time</b></h1>
+          </div>
+
+          <div class="main-list">
+            <v-list-item>
+              <v-list-item-content>
+                <a href="#"
+                  ><v-list-item-title class="pink--text"
+                    >I am new here</v-list-item-title
+                  ></a
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <a href="#"
+                  ><v-list-item-title class="pink--text"
+                    >I want to change the world</v-list-item-title
+                  ></a
+                >
+              </v-list-item-content>
+            </v-list-item>
+            <v-list-item>
+              <v-list-item-content>
+                <a href="#"
+                  ><v-list-item-title class="pink--text"
+                    >I want to know how to help</v-list-item-title
+                  ></a
+                >
+              </v-list-item-content>
+            </v-list-item>
+          </div>
+
+          <div class="buttons">
+            <img
+              :src="require('../public/images/button1.png')"
+            />
+            <img
+              :src="require('../public/images/button2.png')"
+            />
+          </div>
+        </div>
+      </v-main>
+    </v-app>
+  </div>
 </template>
 
+
 <script>
-import HelloWorld from './components/HelloWorld';
+// import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
 
   components: {
-    HelloWorld,
+    // HelloWorld,
   },
 
-  data: () => ({
-    //
-  }),
+  data: () => ({ drawer: false }),
 };
 </script>
+
+
+<style>
+#app {
+  /* background-position: center center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-image: url("../public/images/home-image.jpg"); */
+  background-color: yellow;
+}
+
+.container {
+  position: relative;
+  text-align: center;
+  top: 50%;
+}
+
+.main-list > v-list-item-title {
+  color: white;
+}
+
+a {
+  text-decoration: none;
+}
+/* mobile */
+@media (max-width: 480px) {
+  #app {
+    background-image: url("../public/images/home-image-mobile.jpeg");
+  }
+}
+
+/* tablet */
+@media (min-width: 481px) and (max-width: 1024px) {
+  body {
+    background-image: url("../public/images/home-image.jpg");
+  }
+}
+
+/* desktop */
+@media (min-width: 1025px) {
+  body {
+    background-image: url("../public/images/home-image.jpg");
+  }
+}
+</style>
