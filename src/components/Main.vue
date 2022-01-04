c0398b0b0b8e0473a4a33700e1591119b5acba89
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-01-04 11:20:31 +0600

    changed text align main, removed h1 on mobile devices

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,20 +1,24 @@
 <template>
   <v-container>
     <div class="container">
-      <div>
-        <h1><b>Let's change the world one campaign at a time</b></h1>
+      <div class="main-head-text">
+        <h1 class="text-center"><b>Let's change the world one campaign at a time</b></h1>
       </div>
 
       <div class="main-list">
+        <v-flex >
         <v-list-item v-for="item in items" :key="item">
+          <!-- <v-spacer></v-spacer> 
+          <v-spacer></v-spacer>  -->
           <v-list-item-content>
             <a href="#"
-              ><v-list-item-title class="pink--text"
+              ><v-list-item-title class="pink--text text-left"
                 >{{item}}</v-list-item-title
               ></a
             >
           </v-list-item-content>
         </v-list-item>
+        </v-flex>
         
       </div>
 
@@ -23,6 +27,7 @@
         <a href=""><img :src="require('../assets/images/button2.png')" /></a> 
       </div>
     </div>
+
   </v-container>
 </template>
 
