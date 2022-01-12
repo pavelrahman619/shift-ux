d931d2347eacbb0664b7ed0fff7bee6cce2593df
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-01-12 15:11:47 +0600

    homepage mobile fixed with shapes

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,19 +1,57 @@
 <template>
-  <v-container yellow fluid fill-height1>
-    <v-row>
-      <v-row class="main-head-text" align="center" justify="center">
-        <h1>
-          <b>Let's change the world one campaign at a time</b>
-        </h1>
-      </v-row>
+  <v-container class="yellow" fluid fill-height1>
+    <row class="main-head-text">
+      <h1 class="text-center">
+        <b>Let's change the world one campaign at a time</b>
+      </h1>
+    </row>
 
-
+    <v-row class="justify-center pa-2">
+      <div class="main-shape-1"></div>
     </v-row>
+    <v-row class="pa-2">
+      <v-card width="100%" rounded>
+        <v-img src="@/assets/images/homepage/1.png" height="auto"> </v-img>
+      </v-card>
+    </v-row>
+
+    <div class="main-list">
+      <v-flex shrink>
+        <v-list-item v-for="item in items" :key="item">
+          <!-- <v-spacer></v-spacer> 
+        <v-spacer></v-spacer>  -->
+          <v-list-item-content>
+            <a href="#"
+              ><v-list-item-title class="pink--text text-left">{{
+                item
+              }}</v-list-item-title></a
+            >
+          </v-list-item-content>
+        </v-list-item>
+      </v-flex>
+    </div>
+
+    <div class="buttons-list">
+      <!-- <a href=""><img :src="require('../assets/images/button1.png')" /></a> 
+      <a href=""><img :src="require('../assets/images/button2.png')" /></a>  -->
+      <a href="">
+        <div class="rectangle">
+          <div class="circle"></div>
+          <b><p class="button-list-count">5</p></b>
+        </div></a
+      >
+      <a href=""
+        ><div class="circle">
+          <v-icon color="white" size="20px">mdi-chat-processing-outline</v-icon>
+        </div></a
+      >
+      <div></div>
+    </div>
   </v-container>
 </template>
 
 <script>
-import mainListItems from '../mixins/mainListItems';
+import mainListItems from "../mixins/mainListItems";
 export default {
   name: "Main",
   // data: function () {
@@ -25,13 +63,9 @@ export default {
   //     ],
   //   };
   // },
-  mixins: [mainListItems]
+  mixins: [mainListItems],
 };
 </script>
 
 <style lang="scss">
-// .container {
-//   padding: 5em 0 5em 0;
-// }
 </style>
-
