a06fd3692bb67489ebd5d77584970a008ba94d83
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-01-12 11:43:53 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -12,11 +12,11 @@
         <!-- <v-container fluid> -->
 
           <!--  -->
-          <!-- <Main></Main> -->
+          <Main></Main>
           <!-- <MakeAPlan1></MakeAPlan1> -->
           <!-- <MakeAPlan2></MakeAPlan2> -->
           <!-- <MakeAPlan3></MakeAPlan3> -->
-          <MakeAPlan31></MakeAPlan31>
+          <!-- <MakeAPlan31></MakeAPlan31> -->
           <!-- <Menu /> -->
         <!-- </v-container> -->
       </v-main>
@@ -28,11 +28,11 @@
 
 <script>
 import Header from "./components/Header";
-// import Main from "./components/Main";
+import Main from "./components/Main";
 // import MakeAPlan1 from "./components/MakeAPlan1";
 // import MakeAPlan2 from "./components/MakeAPlan2";
 // import MakeAPlan3 from "./components/MakeAPlan3";
-import MakeAPlan31 from "./components/MakeAPlan31";
+// import MakeAPlan31 from "./components/MakeAPlan31";
 import Footer from "./components/Footer";
 
 export default {
@@ -40,11 +40,11 @@ export default {
 
   components: {
     Header,
-    // Main,
+    Main,
     // MakeAPlan1,
     // MakeAPlan2,
     // MakeAPlan3,
-    MakeAPlan31,
+    // MakeAPlan31,
     Footer,
   },
 
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,39 +1,14 @@
 <template>
-  <v-container>
-    <div class="container">
-      <div class="main-head-text">
-        <h1 class="text-center">
+  <v-container yellow fluid fill-height1>
+    <v-row>
+      <v-row class="main-head-text" align="center" justify="center">
+        <h1>
           <b>Let's change the world one campaign at a time</b>
         </h1>
-      </div>
+      </v-row>
 
-      <div class="main-list">
-        <v-flex shrink>
-          <v-list-item v-for="item in items" :key="item">
-            <!-- <v-spacer></v-spacer> 
-          <v-spacer></v-spacer>  -->
-            <v-list-item-content>
-              <a href="#"
-                ><v-list-item-title class="pink--text text-left">{{
-                  item
-                }}</v-list-item-title></a
-              >
-            </v-list-item-content>
-          </v-list-item>
-        </v-flex>
-      </div>
 
-      <div class="buttons-list">
-        <!-- <a href=""><img :src="require('../assets/images/button1.png')" /></a> 
-        <a href=""><img :src="require('../assets/images/button2.png')" /></a>  -->
-        <a href="">
-          <div class="rectangle">
-            <div class="circle">
-              </div><b><p class="button-list-count">5</p></b></div></a>
-        <a href=""><div class="circle"><v-icon color="white" size="20px">mdi-chat-processing-outline</v-icon></div></a>
-        <div></div>
-      </div>
-    </div>
+    </v-row>
   </v-container>
 </template>
 
@@ -55,7 +30,8 @@ export default {
 </script>
 
 <style lang="scss">
-.container {
-  margin: 5em 0 5em 0;
-}
+// .container {
+//   padding: 5em 0 5em 0;
+// }
 </style>
+
