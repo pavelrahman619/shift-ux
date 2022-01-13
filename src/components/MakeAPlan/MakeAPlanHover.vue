9a5c34a80f8b8bf0151227f393f587a47a32663a
pavelrahman619 <96363207+pavelrahman619@users.noreply.github.com>
2022-01-13 15:17:08 +0600

    after crash

--- src/components/MakeAPlan/MakeAPlanHover.vue
+++ src/components/MakeAPlan/MakeAPlanHover.vue
@@ -1,15 +1,40 @@
 <template>
   <v-container fluid fill-height1 class="yellow lighten-5">
-    <v-card class="border2" height="100%">
-      <v-text-field v-for="i in 4" :key="i" :label="`Problem ${i}`" required></v-text-field>
-    </v-card>
-    <div class="makeaplan-hover-shape-mobile"><p>I need help</p></div>
+    <v-navigation-drawer
+      right
+      hide-overlay="true"
+      v-model="helpDrawer"
+      width="100%"
+      height="100%"
+      absolute
+    >
+      <MakeAPlanHoverMenu></MakeAPlanHoverMenu>
+    </v-navigation-drawer>
 
+    <v-card class="border2" height="100%">
+      <v-text-field
+        v-for="i in 4"
+        :key="i"
+        :label="`Problem ${i}`"
+        required
+      ></v-text-field>
+    </v-card>
+    <div @click="helpDrawer = !helpDrawer" class="makeaplan-hover-shape-mobile">
+      <p>I need help</p>
+    </div>
   </v-container>
 </template>
 
 <script>
-export default {};
+import MakeAPlanHoverMenu from "@/components/MakeAPlan/MakeAPlanHoverMenu";
+
+export default {
+  components: {
+    MakeAPlanHoverMenu,
+  },
+
+  data: () => ({ helpDrawer: false }),
+};
 </script>
 
 <style>
---
+++ src/components/MakeAPlan/MakeAPlanHoverMenu.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container class="pink white--text" fluid fill-height1>
+    <v-row class="ma-5">
+      <v-col cols="12" lg="6">
+        <v-row justify="center">
+          <iframe
+            width="100%"
+            height="100%"
+            src="https://www.youtube.com/embed/nhBVL41-_Cw"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </v-row>
+        <v-row class="font-weight-bold">
+          <p>Title of the video</p>
+        </v-row>
+        <v-row>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+        </v-row>
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-btn rounded outlined class="white--text">
+        <v-icon class="white--text">mdi-refresh</v-icon>
+        Show me more</v-btn
+      >
+      <a href="#">Go to guide page</a>
+      <v-btn class="pink white--text">Go to Guide Page</v-btn>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "MakeAPlanHoverMenu",
+
+  data: () => ({}),
+};
+</script>
